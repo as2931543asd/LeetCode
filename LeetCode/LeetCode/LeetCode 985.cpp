6197632364c954e11341ca6e1db200025ddd2c08
涛 江 <szhx20208@outlook.com>
2020-09-23 20:41:28 +0800

    add

---
+++ LeetCode/LeetCode/LeetCode 985.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution1 {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
+        vector<int> answer;
+        int count = 0;
+        for (size_t i = 0; i < A.size(); i++) {
+            if (A[i] % 2 == 0)
+                count += A[i];
+        }
+        for (int i = 0; i < queries.size(); i++) {
+            if ((A[queries[i][1]] + queries[i][0]) % 2 == 0 && A[queries[i][1]] % 2 == 0) {
+                A[queries[i][1]] += queries[i][0];
+                count += queries[i][0];
+            }
+            else if ((A[queries[i][1]] + queries[i][0]) % 2 == 0 && A[queries[i][1]] % 2 != 0) {
+                A[queries[i][1]] += queries[i][0];
+                count += A[queries[i][1]];
+            }
+            else if ((A[queries[i][1]] + queries[i][0]) % 2 != 0 && A[queries[i][1]] % 2 == 0) {
+                count -= A[queries[i][1]];
+                A[queries[i][1]] += queries[i][0];
+            }
+            else
+                A[queries[i][1]] += queries[i][0];
+            answer.push_back(count);
+        }
+        return answer;
+    }
+
+};
